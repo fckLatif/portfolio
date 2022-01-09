26f452367698699e83d3ae7abfcb5987c8e38134
CreepWannabe <contact@creep.cat>
2022-01-09 21:05:38 +0100

    proper fontawesome config

---
+++ src/fontawesome.js
@@ -0,0 +1,24 @@
+import {
+	faVial,
+	faSnowflake,
+	faHome,
+	faUserSecret,
+	faAddressCard,
+	faProjectDiagram,
+	faUserAstronaut,
+	faDoorOpen,
+	faFingerprint
+} from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+library.add(
+	faVial,
+	faSnowflake,
+	faHome,
+	faUserSecret,
+	faAddressCard,
+	faProjectDiagram,
+	faUserAstronaut,
+	faDoorOpen,
+	faFingerprint
+);
