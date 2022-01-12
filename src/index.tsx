c94d28f13dfbc5ddf0a955402f929794c9e18529
CreepWannabe <contact@creep.cat>
2022-01-12 19:09:10 +0100

    added a loading animation to the site to compensate for db load time

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-
-ReactDOM.render(
-	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
-	</React.StrictMode>,
-	document.getElementById('root')
+setTimeout(() =>
+	ReactDOM.render(
+		<React.StrictMode>
+			<Router>
+				<App />
+			</Router>
+		</React.StrictMode>,
+		document.getElementById('root')
+	), 1000
 );
