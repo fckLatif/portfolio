6d7cd9ae035cad9738b1930adab813bc13591db9
CreepWannabe <contact@creep.cat>
2022-01-16 16:23:55 +0100

    moved contact to its own folder

---
+++ src/routes/contact/Contact.tsx
@@ -0,0 +1,15 @@
+import { StyledPageTitle } from "../../components/styled/StyledH2";
+import ContactContent from "./ContactContent";
+
+const Contact = () => {
+	return (
+		<>
+			<StyledPageTitle>cøntact</StyledPageTitle>
+			<div className="project-page">
+				<ContactContent />
+			</div>
+		</>
+	);
+}
+
+export default Contact
