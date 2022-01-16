47a42503d78de1a6ee031b5a523529b409bd8aaf
CreepWannabe <contact@creep.cat>
2022-01-16 16:24:08 +0100

    updated routeconfig according to latest changes

--- src/routes/RouteConfig.tsx
+++ src/routes/RouteConfig.tsx
@@ -1,57 +1,57 @@
 import Home from './Home';
-import Contact from './Contact';
+import Contact from './contact/Contact';
 import About from './About';
-import Projects from './Projects'
 import PageNotFound from './PageNotFound';
 import SignIn from './SignIn';
-import Admin from './Admin';
+import AdminHome from './admin/AdminHome';
 import SignOut from './SignOut';
+import AdminCreate from './admin/AdminCreate';
+import Projects from './projects/Projects';
 
 import { Navigate } from 'react-router-dom';
 
 const RouteConfig = ({ isAuth }: {
-	isAuth: boolean;
+	isAuth: any;
 }) => [
 		{
 			path: '/',
-			children: [
-				{
-					index: true,
-					element: <Home />
-				},
-				{
-					path: '/about',
-					element: <About />
-				},
-				{
-					path: '/projects',
-					element: <Projects />
-				},
-				{
-					path: '/contact',
-					element: <Contact />
-				},
-				{
-					path: '/signin',
-					element: isAuth ? <Navigate to='/admin' /> : <SignIn />
-				},
-				{
-					path: '/signout',
-					element: isAuth ? <SignOut /> : <Navigate to='/signin' />
-				},
-			],
+			element: <Home />
+		},
+		{
+			path: '/about',
+			element: <About />
+		},
+		{
+			path: '/projects',
+			element: <Projects />
+		},
+		{
+			path: '/contact',
+			element: <Contact />
+		},
+		{
+			path: '/signin',
+			element: isAuth ? <Navigate to='/admin' /> : <SignIn />
+		},
+		{
+			path: '/signout',
+			element: isAuth ? <SignOut /> : <Navigate to='/signin' />
 		},
 		{
 			path: '*',
 			element: <PageNotFound />
 		},
 		{
-			path: 'admin',
+			path: '/admin/',
 			children: [
 				{
 					index: true,
-					element: isAuth ? <Admin /> : <Navigate to='/signin' />,
-				}
+					element: isAuth ? <AdminHome /> : <Navigate to='/signin' />,
+				},
+				{
+					path: 'create',
+					element: isAuth ? <AdminCreate /> : <Navigate to='/signin' />,
+				},
 			]
 		}
 	];
